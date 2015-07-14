66293eea8f443fefc1b4d9fabdcd0e7f654de32c
岳大禹 <yuedayu13@gmail.com>
2015-07-14 18:36:23 +0800

    实现图片懒加载错误处理

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,13 @@ $(function(){
     console.log(data);
   });
   window.BackToTop.init({RightDown: true});
-  $('#ul1 img').lazyload({event: 'scrollstop' ,effect:'fadeIn'});
+  $('#ul1 img').lazyload({event: 'scrollstop', effect:'fadeIn', error: function() {
+    this.src = './images/error.jpg';
+  }});
   $('#ul2 img').lazyload({event: 'scrollstop' ,effect:'fadeIn'});
   $('#ul3 img').lazyload({event: 'scrollstop' ,effect:'fadeIn'});
   $('#ul4 img').lazyload({event: 'scrollstop' ,effect:'fadeIn'});
+  $('img').onload = function() {
+    console.log('hello');
+  }
 });
