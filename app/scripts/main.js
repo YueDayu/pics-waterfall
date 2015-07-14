2f8f3e0772d62b3d4672488c36724c82236a77d3
岳大禹 <yuedayu13@gmail.com>
2015-07-14 15:40:53 +0800

    初步成型

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,1 @@
-console.log('\'Allo \'Allo!');
+window.BackToTop.init({RightDown: true});
---
+++ app/scripts/scroll.js
@@ -0,0 +1,92 @@
+/**
+ * Created by Yue Dayu on 2015/7/12.
+ */
+
+;(function(){
+    'use strict';
+
+    var hasClassName = function (inElement, inClassName)
+    {
+        var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
+        return regExp.test(inElement.className);
+    };
+
+    var addClassName = function (inElement, inClassName)
+    {
+        if (!hasClassName(inElement, inClassName)) {
+          inElement.className = [inElement.className, inClassName].join(' ');
+        }
+    };
+
+    var removeClassName = function (inElement, inClassName)
+    {
+        if (hasClassName(inElement, inClassName)) {
+            var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)', 'g');
+            var curClasses = inElement.className;
+            inElement.className = curClasses.replace(regExp, ' ');
+        }
+    };
+
+    var max = function(num1, num2) {
+        return num1 > num2 ? num1 : num2;
+    };
+
+    var getScrollTop = function() {
+        if (document.body.scrollTop) {
+            return document.body.scrollTop;
+        }
+        if (document.documentElement.scrollTop) {
+            return document.documentElement.scrollTop;
+        }
+        return 0;
+    };
+
+    var display = function() {
+        if (getScrollTop() < 30) {
+            addClassName(this.controller, 'hidden');
+        } else {
+            removeClassName(this.controller, 'hidden');
+        }
+    };
+
+    var ToTop = function() {
+        this.controller = null;
+        this.scrollToTop = function() {
+            if (getScrollTop() > 5) {
+                window.scrollTo(0, max((getScrollTop() - 90), 0));
+                setTimeout(function(){return 'BackToTop.scrollToTop()';}, 10);
+            }
+        };
+        this.init = function(style) {
+            this.controller = document.createElement('div');
+            this.controller.setAttribute('id', 'to-top-btn');
+            this.controller.innerHTML = 'top';
+            if (style.LeftUp && style.LeftUp === true) {
+                this.controller.setAttribute('class', 'left-up');
+            } else if (style.LeftDown && style.LeftDown === true) {
+                this.controller.setAttribute('class', 'left-down');
+            } else if (style.RightUp && style.RightUp === true) {
+                this.controller.setAttribute('class', 'right-up');
+            } else if (style.RightDown && style.RightDown === true) {
+                this.controller.setAttribute('class', 'right-down');
+            } else if (style.x && style.y) {
+                this.controller.setAttribute('style', 'left:' + style.x + 'px;top:' + style.y + 'px;');
+            }
+            console.log(getScrollTop());
+            if (getScrollTop() < 20) {
+                addClassName(this.controller, 'hidden');
+            }
+            document.body.appendChild(this.controller);
+            document.onkeydown = function(keyEvent) {
+                if (keyEvent.which === 38 && keyEvent.ctrlKey) {
+                    window.BackToTop.scrollToTop();
+                }
+            };
+            window.addEventListener('scroll', display.bind(this));
+            this.controller.addEventListener('click', this.scrollToTop.bind(this));
+        };
+    };
+
+    window.BackToTop = new ToTop();
+})();
+
